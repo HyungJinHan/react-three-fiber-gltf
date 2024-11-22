a18ab405194864fbdf5529a29fa775eb3b1a10e3
HyungJinHan <han1210_36@naver.com>
2024-11-22 18:08:18 +0900

    그림자 위치 수정

--- src/TripodCanvas.tsx
+++ src/TripodCanvas.tsx
@@ -43,7 +43,7 @@ export const TripodCanvas = () => {
       </Bvh>
       <mesh
         rotation={[-Math.PI / 2, 0, 0]}
-        position={[0, -0.7, 0]}
+        position={[0, -0.6, 0]}
         receiveShadow>
         <planeGeometry args={[100, 100]} />
         <shadowMaterial transparent opacity={0.4} />
